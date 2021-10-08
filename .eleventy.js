e7f8229fe8aa19d067a366c2649cc43814166f20
Begerad, Stefan <stefan@begerad.de>
2021-10-08 10:28:49 -0400

    feat: introduced Nunjucks template language

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,19 @@
-//eleventy config file
-module.exports = function(eleventyConfig) {
-    //the addPassthroughCopy function takes globs of any sort
-    //and can be used multiple times
-    eleventyConfig.addPassthroughCopy("style");
-    //get eleventy to notice changes in the stylce dir
-    eleventyConfig.addWatchTarget("style");
+//11ty config file
+module.exports = config => {
+
+    //returns some settings
+    return {
+
+	//tell 11ty that markdown, data and HTML files are processed by Nunjucks.js
+	markdownTemplateEngine: 'njk',
+	dataTemplateEngine: 'njk',
+	htmlTemplateEngine: 'njk',
+
+	//tell 11ty to look in the src folder for content, templates and other source code and
+	//use dist as output folder
+	dir: {
+	    input: 'src',
+	    output: 'dist'
+	}
+    };
 };
